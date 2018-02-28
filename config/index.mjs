6e0ba2578491dec4242dda0132bdcdd012d8ab78
bvego <bvego00@fesb.hr>
2018-02-28 12:22:01 +0100

    Added redis and docker

--- config/index.mjs
+++ config/index.mjs
@@ -1,5 +1,6 @@
 export const PORT = 5000;
-export const DB_CONNECTION = 'mongodb://localhost:27017';
+export const DB_CONNECTION = 'mongodb://mongo:27017';
+export const REDIS_CONNECTION = 'redis://redis:6379';
 export const GOOGLE_ID = '723860365140-n84h96s612vhcf25tf7ic25q7cnnb8fb.apps.googleusercontent.com';
 export const GOOGLE_SECRET = 'tzT0hYb4imxMZ4N6hLbskQKp';
 export const FACEBOOK_ID = '213243595893523';
--- index.mjs
+++ index.mjs
@@ -1,15 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import redis from 'redis';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import compression from 'compression';
 import api from './routes/api';
 import auth from './routes/auth';
 import initializePassport from './passport';
-import { PORT, DB_CONNECTION } from './config';
+import { PORT, DB_CONNECTION, REDIS_CONNECTION } from './config';
 
 mongoose.connect(DB_CONNECTION);
+
+const redisClient = redis.createClient(REDIS_CONNECTION);
 
 const app = express();
 
