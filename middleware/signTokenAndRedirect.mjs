29c4d34926f9d33385ecf5337796e3a883ec3115
bvego <bvego00@fesb.hr>
2018-03-04 23:51:24 +0100

    Added redis base

--- middleware/signTokenAndRedirect.mjs
+++ middleware/signTokenAndRedirect.mjs
@@ -1,9 +1,13 @@
 import jwt from 'jsonwebtoken';
+import redis from '../utils/redis';
 import config from '../config';
+import { loginByToken } from '../services/user';
 
 // Sign a token and redirect the user to the frontend route with the token in query params
 const signTokenAndRedirect = (req, res, next) => {
   const token = jwt.sign(req.user, config.get('jwtSecret'), { expiresIn: config.get('jwtExpiration') });
+
+  loginByToken(token, req.user);
 
   res.redirect(`${config.get('frontendUrl')}/callback?token=${token}`);
 };
--- services/user.mjs
+++ services/user.mjs
@@ -1,4 +1,7 @@
+import timestring from 'timestring';
+import config from '../config';
 import users from '../models/user';
+import redis, { getAsync } from '../utils/redis';
 
 export const createUser = userData => users.create(userData);
 
@@ -7,3 +10,9 @@ export const findByEmail = email => users.findOne({ email });
 export const findById = id => users.findById(id);
 
 export const updateById = (id, params) => users.update({ _id: id }, params);
+
+export const logoutUserByToken = token => redis.del(token);
+
+export const loginByToken = (token, user) => redis.set(token, user, 'EX', +timestring(config.get('jwtExpiration')));
+
+export const isLoggedIn = async token => !!await getAsync(token);
