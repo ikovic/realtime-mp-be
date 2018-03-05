086c18eb2d3bf1acd7424be677e3465fbc5a4120
bvego <bvego00@fesb.hr>
2018-03-05 10:27:53 +0100

    Small change

--- routes/auth.mjs
+++ routes/auth.mjs
@@ -18,7 +18,7 @@ router.get('/facebook', passport.authenticate('facebook', { scope: ['email'] }))
 router.get('/facebook/callback', passport.authenticate('facebook'), signTokenAndRedirect);
 
 router.post('/logout', validateTokenInMemory, (req, res) => {
-  logoutUserByToken(req.body.token);
+  logoutUserByToken(req.headers.authorization);
   res.send(200);
 });
 
