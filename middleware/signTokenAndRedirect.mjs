51823085a1726e01704b2748f2454d261a6e880d
bvego <bvego00@fesb.hr>
2018-03-05 11:10:14 +0100

    Added some comments

--- middleware/signTokenAndRedirect.mjs
+++ middleware/signTokenAndRedirect.mjs
@@ -3,7 +3,12 @@ import redis from '../utils/redis';
 import config from '../config';
 import { loginByToken } from '../services/user';
 
-// Sign a token and redirect the user to the frontend route with the token in query params
+/**
+ * Sign a token and redirect the user to the frontend route with the token in query params
+ * @param {object} req - Request object
+ * @param {object} res - Response object
+ * @param {function} next - Calls the next middleware
+ */
 const signTokenAndRedirect = (req, res, next) => {
   const token = jwt.sign(req.user, config.get('jwtSecret'), { expiresIn: config.get('jwtExpiration') });
 
--- middleware/validateTokenInMemory.mjs
+++ middleware/validateTokenInMemory.mjs
@@ -1,5 +1,11 @@
 import { isValidToken } from '../services/user';
 
+/**
+ * Checks whether the token supplied in the authorization header exists in the active redis instance
+ * @param {object} req - Request object
+ * @param {object} res - Response object
+ * @param {function} next - Calls the next middleware in the chain
+ */
 export default async (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) {
